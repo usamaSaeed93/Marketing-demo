f3f8fcba5ea299a28c3c34708b64c198c91dc917
kembourne <kembourne93@gmail.com>
2023-07-11 19:39:19 +0500

    page issue resolved.

--- app/log-in/page.tsx
+++ app/log-in/page.tsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import Side from '@/Components/Form/FormSide/Side'
-import Footer from '@/Layout/Footer'
-import LogInForm from '@/Components/Form/LogInForm'
-import FormNavbar from '@/Layout/FormNavbar'
+import React from "react";
+import Side from "@/Components/Form/FormSide/Side";
+import Footer from "@/Layout/Footer";
+import LogInForm from "@/Components/Form/LogInForm";
+import FormNavbar from "@/Layout/FormNavbar";
+
+
 export default function page() {
   return (
     <div className="flex flex-col">
-    <div className="flex flex-col lg:flex-row">
-    <div className="absolute left-0 right-0 lg:left-[5%] w-full lg:right-[5%] ">
-        <FormNavbar />
+      <div className="flex flex-col lg:flex-row">
+        <div className="absolute left-0 right-0 lg:left-[5%] w-full lg:right-[5%] ">
+          <FormNavbar />
+        </div>
+        <LogInForm />
+        <Side />
       </div>
-      <LogInForm />
-      <Side />
+      <Footer />
     </div>
-<Footer />
-    </div>
-  )
+  );
 }
